d1281046cbb248ac5e777cc4127375c3e0bc5538
Lokendra <lokendra20048@gmail.com>
2025-05-14 14:19:18 +0530

    intall bun and make header

---
+++ src/pages/misc/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="flex justify-between items-center p-4 bg-primary">
+            <h1 className="text-2xl font-bold text-white">
+                Himwan Refrigerations
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
