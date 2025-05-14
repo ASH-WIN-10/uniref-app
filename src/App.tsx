0f57ce397bae67056159c1864db4464d797efc26
Ashvin Jangid <ashvinjangid10@gmail.com>
2025-05-14 11:34:08 +0530

    setup taiwind and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
     return (
         <div>
             <h1>Welcome to the React App</h1>
             <p>This is a simple React application.</p>
+            <Button>Click me</Button>
         </div>
     )
 }
