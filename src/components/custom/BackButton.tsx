db67007ba541540644e32a0d27c3b5bd32f8d7b6
Lokendra <lokendra20048@gmail.com>
2025-05-16 12:59:02 +0530

    change go back arrow styles

--- src/components/custom/BackButton.tsx
+++ src/components/custom/BackButton.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
-
+import { ArrowLeft } from "lucide-react";
 const BackButton = () => {
     return (
-        <div className="mt-10 flex justify-center">
+        <div className="mt-3 flex justify-center">
             <Link to="/">
-                <Button>Go Back</Button>
+                <Button>
+                    <ArrowLeft />
+                </Button>
             </Link>
         </div>
     );
